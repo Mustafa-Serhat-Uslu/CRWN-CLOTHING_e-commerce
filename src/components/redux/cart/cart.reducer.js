64ee6a09cea36122fbb07687b4667d34eb58240a
unknown <uslu.mserhat2@gmail.com>
2022-02-02 08:31:59 +0300

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/redux/cart/cart.reducer.js
+++ src/components/redux/cart/cart.reducer.js
@@ -2,26 +2,25 @@ import CartActionTypes from "./cart.types";
 import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
-  hidden: true,
-  cartItems: [],
-};
-
-//In order for change detetion to be triggered, have to return a new object! each time cant render single prop changes?
-const cartReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case CartActionTypes.TOGGLE_CART_HIDDEN:
-      return {
-        ...state,
-        hidden: !state.hidden,
-      };
-    case CartActionTypes.ADD_ITEM:
-      return {
-        ...state,
-        cartItems: addItemToCart(state.cartItems, action.payload)
-      };
-    default:
-      return state;
-  }
-};
+    hidden: true,
+    cartItems: []
+  };
+  
+  const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case CartActionTypes.TOGGLE_CART_HIDDEN:
+        return {
+          ...state,
+          hidden: !state.hidden
+        };
+      case CartActionTypes.ADD_ITEM:
+        return {
+          ...state,
+          cartItems: addItemToCart(state.cartItems, action.payload)
+        };
+      default:
+        return state;
+    }
+  };
 
 export default cartReducer;
--- src/components/redux/cart/cart.utils.js
+++ src/components/redux/cart/cart.utils.js
@@ -2,7 +2,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   // if the item doesnt already exist, existingCartItem will be undefined
   const existingCartItem = cartItems.find(
-    (cartItem) => (cartItem) => cartItem.id == cartItemToAdd.id
+    (cartItem) => cartItem.id == cartItemToAdd.id
   );
 
   // map funs retrun a completely new object hence it will reuire the component to re-render as needed
@@ -14,5 +14,6 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
       );
   }
 
+  //baseQuantity: 1
   return [...cartItems, {...cartItemToAdd, quantity: 1}];
 };
